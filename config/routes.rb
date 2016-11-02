ce35049d0ac52cad70211f09077e69291e7069f6
Marius Turcanu <gomacroice@gmail.com>
2016-11-02 13:39:58 +0000

    TestPentruHer

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   end
   
   get '/about' => 'pages#about'
+  get '/products' => 'pages#products'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
